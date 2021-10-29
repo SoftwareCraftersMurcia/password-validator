e89d471b2b0756908b9921bca5ee6ace24287814
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-10-29 19:27:53 +0200

    Add a pattern builder password

--- php/tests/PasswordValidatorTest.php
+++ php/tests/PasswordValidatorTest.php
@@ -15,11 +15,17 @@ final class PasswordValidatorTest extends TestCase
     - Contains a number
     - Contains an underscore
      */
+
+    private const MORE_THAN_8_CHARS = "########";
+    private const CAPITAL_LETTER = "A";
+    private const LOWER_CASE = "b";
+    private const NUMBER = "1";
+    private const UNDERSCORE = "_";
     
     /** @test */
     public function should_validate_password_is_longer_than_eight_chars(): void
     {
-        $password = 'Aeioua1_jsdf';
+        $password = self::MORE_THAN_8_CHARS . self::CAPITAL_LETTER . self::LOWER_CASE . self::NUMBER . self::UNDERSCORE;
         $passwordValidator = new PasswordValidator();
         $this->assertTrue($passwordValidator->validate($password));
     }
@@ -27,7 +33,7 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_not_be_valid_less_than_eight_char(): void
     {
-        $password = '123';
+        $password = self::CAPITAL_LETTER . self::LOWER_CASE . self::NUMBER . self::UNDERSCORE;
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
@@ -35,7 +41,7 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_have_at_least_capital_letter(): void
     {
-        $password = 'eioua1_jsdf';
+        $password = self::MORE_THAN_8_CHARS . self::LOWER_CASE . self::NUMBER . self::UNDERSCORE;
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
@@ -43,7 +49,7 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_have_at_one_lower_case_letter(): void
     {
-        $password = 'AEIOUA1_JSDF';
+        $password = self::MORE_THAN_8_CHARS . self::CAPITAL_LETTER . self::NUMBER . self::UNDERSCORE;
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
@@ -51,7 +57,7 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_have_at_least_one_number(): void
     {
-        $password = 'Aeioua_jsdf';
+        $password = self::MORE_THAN_8_CHARS . self::CAPITAL_LETTER . self::LOWER_CASE . self::UNDERSCORE;
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
@@ -59,7 +65,7 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_have_at_least_one_underscore(): void
     {
-        $password = 'Aeioua1jsdf';
+        $password = self::MORE_THAN_8_CHARS . self::CAPITAL_LETTER . self::LOWER_CASE . self::NUMBER;
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
