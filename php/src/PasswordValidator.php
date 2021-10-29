ab0ed65027f0375dd154683eb28ab62a4681fad5
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-10-29 19:17:09 +0200

    should_have_at_least_one_number

--- php/src/PasswordValidator.php
+++ php/src/PasswordValidator.php
@@ -9,6 +9,7 @@ final class PasswordValidator
     private const MINIMUM_LENGTH = 8;
     private const AT_LEAST_A_CAPITAL_LETTER = '/[A-Z]/';
     private const AT_LEAST_A_LOWER_LETTER = '/[a-z]/';
+    private const AT_LEAST_A_NUMBER = '/[0-9]/';
 
     public function validate(string $password): bool
     {
@@ -18,6 +19,8 @@ final class PasswordValidator
 
         $hasLowerCaseLetter = preg_match(self::AT_LEAST_A_LOWER_LETTER, $password);
 
-        return $hasEnoughLength && $hasCapitalLetter && $hasLowerCaseLetter;
+        $hasANumber = preg_match(self::AT_LEAST_A_NUMBER, $password);
+
+        return $hasEnoughLength && $hasCapitalLetter && $hasLowerCaseLetter && $hasANumber;
     }
 }
--- php/tests/PasswordValidatorTest.php
+++ php/tests/PasswordValidatorTest.php
@@ -35,7 +35,7 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_have_at_least_capital_letter(): void
     {
-        $password = 'a12345678';
+        $password = 'eioua1_jsdf';
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
@@ -43,9 +43,16 @@ final class PasswordValidatorTest extends TestCase
     /** @test */
     public function should_have_at_one_lower_case_letter(): void
     {
-        $password = 'A12345678';
+        $password = 'AEIOUA1_JSDF';
         $passwordValidator = new PasswordValidator();
         $this->assertFalse($passwordValidator->validate($password));
     }
 
+    /** @test */
+    public function should_have_at_least_one_number(): void
+    {
+        $password = 'Aeioua_jsdf';
+        $passwordValidator = new PasswordValidator();
+        $this->assertFalse($passwordValidator->validate($password));
+    }
 }
